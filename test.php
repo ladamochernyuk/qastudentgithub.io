edda6f12c1501f732136ad4b6d1ebc3e321cc901
ladamochernyuk <75637229+ladamochernyuk@users.noreply.github.com>
2021-03-10 21:48:57 +0200

    new

--- test.php
+++ test.php
@@ -6,13 +6,15 @@ require_once __DIR__.'/core/Connection.php';
 $db = Connection::connect();
 
 $sql = <<<SQL
-    DROP TABLE IF EXISTS brands;
-    CREATE TABLE brands(
-        id int(11) unsigned NOT NULL AUTO_INCREMENT,
-        name varchar(255) NOT NULL,
-        status tinyint(1) DEFAULT 1,
-        PRIMARY KEY(id)
-    )
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`(
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`user_id` int(11) unsigned NOT NULL,
+`status` tinyint(1) NOT NULL DEFAULT '1',
+`products` text NOT NULL,
+`order_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY(`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 SQL;
 
 $db->exec($sql);
